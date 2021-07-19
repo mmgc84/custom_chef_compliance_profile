270d917e0bda24b00a3a63d9590ad7b457ae5293
Marcelo Gutierrez <mmgc84@gmail.com>
2021-07-18 19:19:03 -0600

    Added Compliance Profile to chef if Chef is installed and check its version

---
+++ README.md
@@ -0,0 +1,10 @@
+# chef_ws
+
+This compliance Profile ensures that a workstation has the minimum
+required configuration necessary for Chef Development
+
+## Standalone Usage
+```
+$ git clone ...
+$ inspec exec chef_ws
+```
---
+++ controls/default_test.rb
@@ -0,0 +1,27 @@
+control 'Chef Workstation is installed' do
+  describe file('/opt/chef-workstation/bin/chef') do
+    it { should exist }
+  end
+
+  describe command('/opt/chef-workstation/bin/chef --version') do
+    its('stdout') { should match /Chef Workstation version: / }
+  end
+end
+
+control 'Chef Workstation is at least 21.2.278' do
+  chef_version_output = command('/opt/chef-workstation/bin/chef --version').stdout
+  version = chef_version_output.match(/Chef Workstation version: (\d+\.\d+\.\d+)/)[1]
+  major_version = version.split('.')[0].to_i
+  minor_version = version.split('.')[1].to_i
+  patch_version = version.split('.')[2].to_i
+  # puts("'#{major_version}.#{minor_version}.#{patch_version}'")
+  describe(major_version) do
+    it { should be >= 21 }
+  end
+  describe(minor_version) do
+    it { should be >= 2 }
+  end
+  describe(patch_version) do
+    it { should be >= 278 }
+  end
+end
